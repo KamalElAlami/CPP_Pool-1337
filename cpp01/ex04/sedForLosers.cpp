9acd4545987431372cab2effd1d646e0bd7cb382
Kamal El Alami <kelalami123@gmail.com>
2025-02-11 00:39:48 +0100

    cpp1 one fixed

--- cpp01/ex04/sedForLosers.cpp
+++ cpp01/ex04/sedForLosers.cpp
@@ -1,5 +1,5 @@
 #include "sedForLosers.hpp"
-
+#include <cstdlib>
 
 Replace::Replace(std::string s1, std::string s2, std::string file) :
 infile(file),
@@ -7,17 +7,20 @@ outfile(file + ".replace"),
 str1(s1),
 str2(s2)
 {
-	// if (s1.empty() || s2.empty() || file.empty())
-	// 	std::cout << "invalid args" << std::endl;
 }
 
 std::string Replace::findAndReplace(std::string line, std::string word)
 {
+    std::string new_line = line;
+    size_t pos = 0;
 
-	std::string new_line = line;
-	while (new_line.find(str1) != std::string::npos)
-		new_line = new_line.substr(0, new_line.find(str1.c_str())) + word + new_line.substr((new_line.find(str1) + str1.length()), new_line.length());
-	return (new_line);
+    while ((pos = new_line.find(str1, pos)) != std::string::npos)
+    {
+        new_line = new_line.substr(0, pos) + word + 
+                   new_line.substr(pos + str1.length());
+        pos += word.length();
+    }
+    return new_line;
 }
 void Replace::giga_sed(void)
 {
