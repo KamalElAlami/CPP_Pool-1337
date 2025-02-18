c93d321060e92a64faf52547f9cac1f9221491c4
Kamal El Alami <kelalami123@gmail.com>
2025-02-18 01:24:09 +0100

    done with constructors and methods

--- cpp01/ex03/Weapon.cpp
+++ cpp01/ex03/Weapon.cpp
@@ -5,8 +5,9 @@ Weapon::Weapon(std::string Weapon)
 	type = Weapon;
 }
 
-const std::string& Weapon::getType(void)
+const std::string& Weapon::getType(void) const
 {
+	
 	return (type);
 }
 void Weapon::setType(std::string weap)
--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -1,22 +1,38 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed()
+Fixed::Fixed() : fixedPoint(0)
 {
 	std::cout << "Default constructor called" << std::endl;
-	primary = 0;
 }
+
+Fixed::Fixed(int number) : fixedPoint(number << fbits)
+{
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(float number) : fixedPoint(roundf(number * (1 << fbits)))
+{
+	std::cout << "Float constructor called" << std::endl;
+}
+
 Fixed& Fixed::operator=(const Fixed& obj)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &obj)
-		this->primary = obj.primary;
+		this->fixedPoint = obj.fixedPoint;
 	return (*this);
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed& obj)
+{
+	out << obj.toFloat();
+	return (out);
 }
 
 Fixed::Fixed(Fixed& obj)
 {
 	std::cout << "Copy constructor called" << std::endl;
-	this->primary = obj.primary;
+	fixedPoint = obj.fixedPoint;
 }
 
 Fixed::~Fixed()
@@ -27,5 +43,15 @@ Fixed::~Fixed()
 int Fixed::getRawBits(void) const
 {
 	std::cout << "getRawBits member function called" << std::endl;
-	return (primary);
+	return (fixedPoint);
+}
+
+float Fixed::toFloat( void ) const
+{
+	return (fixedPoint / (1 << fbits));
+}
+
+int Fixed::toInt( void ) const
+{
+	return (fixedPoint >> fbits);
 }
