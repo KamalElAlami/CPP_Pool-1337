b10d3dc00648763ffaff5de3277a599ebe20bd6c
kamal el alami <kelalami123@gmail.com>
2025-07-08 09:23:42 +0100

    last exercise in cpp07 is done

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -2,19 +2,64 @@
 #define ARRAY_HPP
 
 #include <iostream>
+#include <exception>
+#include <cstdlib>
 
 template<typename T>
 class Array
 {
     private :
         T*      array;
-        size_t  size;
+        unsigned int  lenght;
     public :
-        Array()
-        Array(unsigned int n);
-        Array(const Array& obj);
-        Array& operator=(const Array& obj);
-        ~Array();
+        Array() : array(NULL), lenght(0)
+        {}
+        Array(unsigned int n) : lenght(n)
+        {
+            array = new T[lenght];
+        }
+        Array(const Array& obj)
+        {
+            *this = obj;
+        }
+        const Array& operator=(const Array& obj) 
+        {
+            if (this != &obj)
+            {
+                this->lenght = obj.lenght;
+                delete[] array;
+                array = new T[lenght];
+                for (size_t i = 0; i < lenght; i++)
+                {
+                    this->array[i] = obj.array[i];
+                }
+            }
+            return (*this);
+        }
+        const T& operator[](unsigned int index) const
+        {
+            if (index > lenght)
+                throw std::runtime_error("Out Of Range Index");
+            retun (array[index]);
+        }
+        T& operator[](unsigned int index)
+        {
+            if (index >= lenght)
+                throw std::runtime_error("Out Of Range Index");
+            return (array[index]);
+        }
+        unsigned int size(void) const
+        {
+            return (lenght);
+        }
+        T& getArray(void) const
+        {
+            return (array);
+        }
+        ~Array()
+        {
+            delete[] array;
+        }
 
 };
 
