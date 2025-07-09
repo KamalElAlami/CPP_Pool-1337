7ca58c4ebf0d59108d36861a22c9bbb80f003d64
kamal el alami <kelalami123@gmail.com>
2025-07-09 16:20:56 +0100

    x

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,6 +6,8 @@
 template<typename T1>
 void iter(T1& array, size_t size, void (*func)(T1&))
 {
+    if (!func)
+        return ;
     for (size_t i = 0; i < size; i++)
     {
         func(array[i])
