4741c518a7262843f3840fd540717487fc4e26dd
Kamal El Alami <kelalami123@gmail.com>
2025-08-22 06:44:47 +0100

    replace virtual pairs with std::pair

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -1,5 +1,5 @@
 #include "PmergeMe.hpp"
-
+#include <utility>
 
 int binarySearch(std::vector<int> vec, int start, int end, int x)
 {
@@ -14,34 +14,44 @@ int binarySearch(std::vector<int> vec, int start, int end, int x)
         return (binarySearch(vec, mid + 1, end, x));
 }
 
-void createMainAndPend(std::vector<int>& vec, std::vector<int>& main, std::vector<int>& pend)
+void createMainAndPend(std::vector<std::pair<int, int> >& vec, std::vector<int>& main, std::vector<int>& pend, int remainder)
 {    
-    for (size_t i = 0; i < vec.size(); i++)
+    for (size_t i = 0; i < vec.size() - 1; i++)
     {
         if (!i)
         {
-            main.push_back(vec[1]);
-            main.push_back(vec[0]);
-            i += 2;
+            main.push_back(vec[i].second);
+            main.push_back(vec[i].first);
         }
-        if (i % 2 == 0)
-            main.push_back(vec[i]);
         else
-            pend.push_back(vec[i]);
+        {
+            main.push_back(vec[i].first);
+            pend.push_back(vec[i].second);
+        }
     }
+    if (remainder != -1)
+        pend.push_back(remainder);
+    std::cout << "Main Chain" << std::endl;
+    for (size_t i = 0; i < main.size() - 1; i++)
+        std::cout << main[i] << " ";
+    std::cout << std::endl;
+    std::cout << "pend Chain" << std::endl;
+    for (size_t i = 0; i < pend.size() - 1; i++)
+        std::cout << pend[i] << " ";
+    std::cout << std::endl;
 }
+
 
 void makePairs(std::vector<int>& vec)
 {
+    std::vector<std::pair<int, int> > pairs;
     std::vector<int> main;
     std::vector<int> pend;
+    int remainder = (vec.size() % 2 == 1) ? vec[vec.size() - 1] : -1;
     for (size_t i = 0; i < vec.size() - 1 ; i += 2)
-    {
-        if (vec[i] < vec[i + 1])
-            std::swap(vec[i], vec[i + 1]);
-    }
-    createMainAndPend(vec, main, pend);
-    std::cout << binarySearch(main, 0, main.size(), 9) << std::endl;
+            pairs.push_back(std::make_pair(std::max(vec[i], vec[i + 1]), std::min(vec[i], vec[i + 1])));
+    std::sort(pairs.begin(), pairs.end());
+    createMainAndPend(pairs, main, pend, remainder);
 }
 
 int main(int ac, char **av)
@@ -60,7 +70,7 @@ int main(int ac, char **av)
         if (flag && number <= INT_MAX && number >= 0)
         container.push_back(number);
         else
-        return (std::cerr << "Error" << std::endl, 1);
+            return (std::cerr << "Error" << std::endl, 1);
     }
     makePairs(container);
 }
